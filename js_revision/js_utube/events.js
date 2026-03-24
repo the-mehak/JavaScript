992dc74e09aa93d927a19920849681e5402e4260
Mahak <thatonemehak@gmail.com>
2026-03-24 22:51:29 +0530

    practice

--- js_revision/js_utube/events.js
+++ js_revision/js_utube/events.js
@@ -10,14 +10,23 @@
 //   }
 // });
 
-let submit = document.querySelector("#submit");
-let colorname = document.querySelector("#colorname");
-let p = document.createElement("p");
+// let submit = document.querySelector("#submit");
+// let colorname = document.querySelector("#colorname");
+// let p = document.createElement("p");
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.style.backgroundColor = `${colorname.value}`;
-  p.innerText = colorname.value;
-  //   p.style.color = colorname.value;
-  document.body.appendChild(p);
+// submit.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   document.body.style.backgroundColor = `${colorname.value}`;
+//   p.innerText = colorname.value;
+//   //   p.style.color = colorname.value;
+//   document.body.appendChild(p);
+// });
+let btnevent = document.getElementById("btnccolor");
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+console.log(random(255));
+btncolor.addEventListener("click", (e) => {
+  const rndcol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndcol;
 });
