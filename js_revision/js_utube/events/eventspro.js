e6381c5401a3d8f8e1b667ee5ced89410a35f646
Mahak <thatonemehak@gmail.com>
2026-03-25 20:16:09 +0530

    practice for make perfect

---
+++ js_revision/js_utube/events/eventspro.js
@@ -0,0 +1,25 @@
+// Add [0] to get the first element with this class
+let maindiv = document.getElementById("maindiv");
+
+let box1 = document.getElementById("box1");
+
+box1.addEventListener(
+  "click",
+  () => {
+    console.log("box1");
+  },
+  true,
+);
+
+maindiv.addEventListener("click", () => {
+  console.log("maindiv");
+});
+
+// Use document.body
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("body");
+  },
+  true,
+);
