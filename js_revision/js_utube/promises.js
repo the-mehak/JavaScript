3e01b1a5dbd2277030ee225b2db7f33662246b78
Mahak <thatonemehak@gmail.com>
2026-04-08 16:49:52 +0530

    Update promises.js

--- js_revision/js_utube/promises.js
+++ js_revision/js_utube/promises.js
@@ -1,11 +1,24 @@
-const promise1 = new Promise(function fun1(resolve, reject) {
-  //   resolve("fullfilled");
-  reject("rejected");
-}).then(
-  (sucess = (value) => {
-    console.log(value);
-  }),
-  (failure = (err) => {
-    console.log(err);
-  }),
+function isdata(item) {
+  const promise1 = new Promise(function fun1(resolve, reject) {
+    if (item) {
+      resolve("solve");
+    } else {
+      // Sirf console.log kafi nahi hai, reject call karna zaroori hai
+      reject("fail");
+    }
+  });
+  return promise1;
+}
+
+let result = isdata("assd"); // Empty string is 'falsy'
+console.log(result);
+
+result.then(
+  (val) => {
+    console.log("Success:", val);
+  },
+  (failure) => {
+    console.log("Error:", failure);
+  }, // Ab ye trigger hoga!
 );
+console.log("end");
