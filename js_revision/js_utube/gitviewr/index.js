11b91301697abc5d9379999f0bded6e6c21fa212
Mahak <thatonemehak@gmail.com>
2026-04-14 02:31:34 +0530

    git profile viewer

--- js_revision/js_utube/gitviewr/index.js
+++ js_revision/js_utube/gitviewr/index.js
@@ -1,5 +1,54 @@
-let user = document.getElementById("searchbtn");
-user.addEventListener("click", () => {
-  let username = document.getElementsByClassName("name").innerHtml;
-  console.log(username);
+const user = document.getElementById("searchbtn");
+
+user.addEventListener("click", async () => {
+  let username = document.getElementById("getname").value;
+  try {
+    let response = await fetch(`https://api.github.com/users/${username}`);
+    let result = await response.json();
+    if (response.ok) {
+      displayUSer(result);
+    } else {
+      alert("USer Not Found!!!");
+    }
+  } catch (error) {
+    alert(error);
+  }
 });
+
+const displayUSer = ({
+  avatar_url,
+  name,
+  bio,
+  public_repos,
+  followers,
+  following,
+}) => {
+  document.querySelector(".secondDiv").innerHTML = ` 
+      <div class="leftdiv">
+        <img src="${avatar_url}" alt="profile" />
+        <h5>${name || "No Name"}</h5>
+        <p>${bio || "No Bio"}</p>
+      </div>
+      <div class="rightdiv">
+        <div class="profileinfo">
+          <div class="follower">
+            <h5>Followers</h5>
+            <p>${followers || 0}</p>
+          </div>
+          <div class="following">
+            <h5>Following</h5>
+            <p>${following || 0}</p>
+          </div>
+          <div class="repo">
+            <h5>Repository</h5>
+            <p>${public_repos || 0}</p>
+          </div>
+        </div>
+         <div class="viewprofile"><button>View profile</button></div>
+      </div>
+      <div><button class="clear">Clear</button></div>`;
+  document.querySelector(".clear").addEventListener("click", () => {
+    document.getElementById("getname").value = "";
+    document.querySelector(".secondDiv").innerHTML = "";
+  });
+};
