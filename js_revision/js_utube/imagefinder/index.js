6ee227ad9190f28ad7f1f1fa24a15036efc91485
Mahak <thatonemehak@gmail.com>
2026-04-16 17:11:14 +0530

    image finder

---
+++ js_revision/js_utube/imagefinder/index.js
@@ -0,0 +1,6 @@
+let keyword = document.getElementById("keyword");
+let searchBtn = document.getElementById("searchbtn");
+
+searchBtn.addEventListener("click", () => {
+  console.log(keyword.value);
+});
