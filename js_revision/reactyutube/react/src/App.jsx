58b1e02cebe9158640c15ab278c588034dcb2615
Mahak <thatonemehak@gmail.com>
2026-04-30 01:23:12 +0530

    react components and props

--- js_revision/reactyutube/react/src/App.jsx
+++ js_revision/reactyutube/react/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 // import "./App.css";
+import Product from "./components/Product";
 
-function App() {
+function App({ shoesdata }) {
   return (
     <>
-      <h1>"Hello"</h1>
+      {shoesdata.map((data, index) => {
+        return <Product key={index} title={data.title} price={data.price} />;
+      })}
     </>
   );
 }
--- js_revision/reactyutube/react/src/main.jsx
+++ js_revision/reactyutube/react/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+let shoesdata = [
+  {
+    title: "nike",
+    price: 5000,
+  },
+  {
+    title: "puma",
+    price: 9000,
+  },
+  {
+    title: "adidas",
+    price: 10000,
+  },
+  {
+    title: "Skechers",
+    price: 15000,
+  },
+];
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <App shoesdata={shoesdata} />
   </StrictMode>,
 );
