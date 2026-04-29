afed82f10e9b7b1e73d8d70db92fb457728d3751
Mahak <thatonemehak@gmail.com>
2026-04-29 21:03:57 +0530

    React

---
+++ js_revision/js_utube/inheritance.js
@@ -0,0 +1,29 @@
+// . Using ES6 Classes (Most common)
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   speak() {
+//     console.log(this.name + " makes a sound");
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name, breed) {
+//     super(name); // call parent constructor
+//     this.breed = breed;
+//   }
+
+//   bark() {
+//     console.log(this.name + " barks");
+//   }
+// }
+
+// const d = new Dog("Buddy", "Labrador");
+// d.speak(); // inherited
+// d.bark(); // own method
+
+// 2. Prototype-based Inheritance (Core JS concept)
+
