2f9a67c2b53006feba0c127ca9afa701573c6b0f
Mahak <thatonemehak@gmail.com>
2026-05-15 19:45:41 +0530

    routinng

---
+++ js_revision/reactyutube/react/src/components/PageNotFound.jsx
@@ -0,0 +1,11 @@
+export function PageNotFound() {
+  return (
+    <>
+      <div className="w-screen h-screen font-sans  bg-slate-300 ">
+        <section className="text-black-300 text-center  mt-30 text-3xl">
+          <h1>Page Not Found</h1>
+        </section>
+      </div>
+    </>
+  );
+}
