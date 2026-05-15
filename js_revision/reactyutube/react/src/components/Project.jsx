ace82e74ab04ae2664be27d562e39b0198dd7beb
Mahak <thatonemehak@gmail.com>
2026-05-15 19:45:49 +0530

    Create Project.jsx

---
+++ js_revision/reactyutube/react/src/components/Project.jsx
@@ -0,0 +1,11 @@
+export function Project() {
+  return (
+    <>
+      <div className="w-screen h-screen font-sans  bg-slate-300 ">
+        <section className="text-black-300 text-center  mt-30 text-3xl">
+          <h1>Projects</h1>
+        </section>
+      </div>
+    </>
+  );
+}
