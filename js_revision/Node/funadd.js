270a4534d17e08ea99b590bcee8e270712b08049
Mahak <thatonemehak@gmail.com>
2026-05-19 20:59:47 +0530

    node basic

---
+++ js_revision/Node/funadd.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+  let sum = a + b;
+  return sum;
+}
+module.exports = add;
--- js_revision/Node/index.js
+++ js_revision/Node/index.js
@@ -1,2 +1,4 @@
-console.log(__dirname.split("\\").slice(-1));
-console.log(__filename.split("\\").slice(-1));
+// console.log(__dirname.split("\\").slice(-1));
+// console.log(__filename.split("\\").slice(-1));
+const addfun = require("./funadd");
+console.log(addfun(2, 7));
